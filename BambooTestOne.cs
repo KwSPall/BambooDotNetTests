001df5fc92ab53b1e8476f557e6b3dcfb27b3569
KwS <jacob.cabaj@gmail.com>
2022-03-10 15:48:20 +0100

    Add project files.

---
+++ BambooTestOne.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BambooDotNetTests
+{
+    [TestClass]
+    public class BambooTestOne
+    {
+        [TestMethod]
+        public void AlwaysTrue()
+        {
+            Assert.AreEqual("asdf", "asdf");
+        }
+
+        [TestMethod]
+        public void NeverTrue()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void SometimesTrue()
+        {
+            Random random = new Random();
+            int v = random.Next(10);
+            if (v == 0)
+            {
+                Assert.Inconclusive();
+            } else
+            {
+                Assert.IsTrue(v < 3);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BambooDotNetTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BambooDotNetTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d5a2fcd-11a1-41af-9002-e6642a426604")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
